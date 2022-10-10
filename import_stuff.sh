7a00a2e882fe65ebc532ab8bf69cbe5cf933ef2e
paulostation <paulostation0@gmail.com>
2022-10-10 13:44:59 -0300

    Import script fixed

--- import_stuff.sh
+++ import_stuff.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-set -x
 set -e
 
 for i in $(yq '.managed_repo_list[]' repos.yml);
-do  
-    echo "$i"
- 
-    bash -c "terraform import -var-file=\"terraform.tfvars\" 'module.branch_protection_rules[\"tweet-analysis\"].github_repository.repository' tweet-analysis"
+do
+    terraform import "module.branch_protection_rules[\"$i\"].github_repository.repository" "$i"
 done
